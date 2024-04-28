5a0e712b2e4429706b7bcd20dc666ee785d1c593
agucasjua <139149489+agucasjua@users.noreply.github.com>
2024-04-28 15:05:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
 export const PORT = process.env.PORT || 3000
+
+export const BD_HOST = process.env.BD_HOST || ''localhost'
+export const DB_USER = process.env.DB_USER || 'root'
+export const BD_PASSWORD = process.env.BD_PASSWORD || 'rkstorepass'
+export const BD_NAME = process.env.BD_NAME || 'usernamedb'
+export const BD_PORT = process.env.BD_PORT || 3306
